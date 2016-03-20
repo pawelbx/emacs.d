415bfb47af9b5c3a819ec78fdc4b47dc3bafcbac
Pawel Bokota <pawelb.lnx@gmail.com>
2016-03-20 18:40:25 -0400

    make magit-status take all space in frame

--- init.el
+++ init.el
@@ -113,3 +113,4 @@
  )
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
--- lisp/init-git.el
+++ lisp/init-git.el
@@ -4,7 +4,11 @@
 (global-git-gutter-mode t)
 (git-gutter:linum-setup)
 
-(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+;(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
 (global-set-key (kbd "C-c s") 'magit-status)
+
+;magit-status will take all space in frame
+(fullframe projectile-vc magit-mode-quit-window)
+(fullframe magit-status magit-mode-quit-window)
 
 (provide 'init-git)
