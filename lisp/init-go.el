74991e38c9d3ca332d1cd0f1e6ae67aeae112ebe
Pawel Bokota <pawelb.lnx@gmail.com>
2016-07-09 10:38:55 -0400

    add company completion for go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,5 +1,10 @@
 (require-package 'go-mode)
 
+(require-package 'company-go)
+
+(require 'company-go)
+(add-to-list 'company-backends 'company-go)
+
 (add-hook 'go-mode-hook 'subword-mode)
 
 (provide 'init-go)
