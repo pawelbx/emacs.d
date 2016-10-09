c966bec1325cd9a8eb7b9dd8da086f69e6910601
Pawel Bokota <pawelb.lnx@gmail.com>
2016-10-09 14:47:21 -0400

    upgrade packages

--- elpa/elfeed-20160826.1719/elfeed-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "elfeed" "20160826.1719" "an Emacs Atom/RSS feed reader"
-  '((emacs "24.3")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/elfeed-20160911.1353/elfeed-pkg.el
@@ -0,0 +1,5 @@
+(define-package "elfeed" "20160911.1353" "an Emacs Atom/RSS feed reader"
+  '((emacs "24.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
