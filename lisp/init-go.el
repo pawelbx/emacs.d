e5eba3cdf7dd09f408703fd0d7c27bc31bd546f7
Pawel Bokota <pawelb.lnx@gmail.com>
2016-10-18 14:06:17 -0400

    fix go paths

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,5 +1,10 @@
 (require-package 'go-mode)
 
+(setenv "PATH" (concat (getenv "PATH") "/home/pbokota/go/bin"))
+(setq exec-path (append exec-path '("/home/pbokota/go/bin")))
+(setenv "GOPATH" "/home/pbokota/go")
+(setq exec-path (append exec-path '("/home/pbokota/go")))
+
 (require-package 'company-go)
 
 (require 'company-go)
