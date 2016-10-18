3785c0e12925bc8907b0d51047cc786e5324ca55
Pawel Bokota <pawelb.lnx@gmail.com>
2016-10-18 14:07:29 -0400

    update packages

---
+++ elpa/magit-popup-20161009.1506/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22529 9948 888368 173000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
