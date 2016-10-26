3a2064b894e8f470faaa6fb7385bf40c7928cc18
Pawel Bokota <pawelb.lnx@gmail.com>
2016-10-26 15:59:04 -0400

    update packages

---
+++ elpa/dash-20161018.136/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22538 20842 887951 736000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ elpa/helm-core-20161020.2305/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20161020.2305" "Development files for Helm"
+  '((emacs "24.4")
+    (async "1.9"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
