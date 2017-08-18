c8dbf9224a4656b79a8d8b4dd317f5e5e562df3b
Pawel Bokota <pawelb.lnx@gmail.com>
2017-08-18 11:19:10 -0400

    set indentation for js

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,5 +1,10 @@
 (require-package 'coffee-mode)
+(require-package 'js2-mode)
 
 (add-hook 'coffee-mode-hook 'subword-mode)
+(add-hook 'js-2-mode-hook 'subword-mode)
+
+(setq js-indent-level 2)
+(setq js2-basic-offset 2)
 
 (provide 'init-javascript)
