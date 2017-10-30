dc61d44dcfc098ef0c5b3aee21cd918da5ae2210
pawel <pawelb.lnx@gmail.com>
2017-10-29 23:15:01 -0400

    simplify haskell mode

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,22 +1,10 @@
 (require-package 'haskell-mode)
 (require-package 'intero)
 
-(require 'haskell)
 (require 'haskell-mode)
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
+(require 'intero)
 
-(eval-after-load "haskell-mode"
-  '(progn
-    (define-key haskell-mode-map (kbd "C-x C-d") nil)
-    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-    (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-    (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-    (define-key haskell-mode-map (kbd "C-c M-.") nil)
-    (define-key haskell-mode-map (kbd "C-c C-d") nil)))
-
-
+(after-load "haskell-mode"
+  (add-hook 'haskell-mode-hook 'intero-mode))
 
 (provide 'init-haskell)
