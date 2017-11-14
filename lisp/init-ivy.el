6e55b5d0a30bf4527ff1b1490a48fc079dca066f
pawel <pawelb.lnx@gmail.com>
2017-11-13 21:43:21 -0500

    switch to ivy from helm

---
+++ lisp/init-ivy.el
@@ -0,0 +1,16 @@
+(require-package 'counsel)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+
+(setq ivy-initial-inputs-alist nil)
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+
+(provide 'init-ivy)
--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,18 +1,18 @@
 (require-package 'projectile)
-(require-package 'helm-projectile)
+(require-package 'counsel-projectile)
 
 (projectile-global-mode t)
 (setq projectile-remember-window-configs t)
-(after-load 'helm
-  (helm-projectile-on))
+
+(counsel-projectile-on)
 
 (setq projectile-mode-line
       '(:eval (format " Projectile[%s(%s)]"
                       (projectile-project-name))))
 
-(define-key projectile-mode-map (kbd "s-<f1>") 'helm-projectile-switch-project)
-(define-key projectile-mode-map (kbd "s-<f2>") 'helm-projectile-find-file)
-(define-key projectile-mode-map (kbd "s-<f3>") 'helm-projectile-ag)
-(define-key projectile-mode-map (kbd "s-<f4>") 'helm-projectile-find-dir)
+(define-key projectile-mode-map (kbd "s-<f1>") 'counsel-projectile-switch-project)
+(define-key projectile-mode-map (kbd "s-<f2>") 'counsel-projectile-find-file)
+(define-key projectile-mode-map (kbd "s-<f3>") 'counsel-projectile-ag)
+(define-key projectile-mode-map (kbd "s-<f4>") 'counsel-projectile-find-dir)
 
 (provide 'init-projectile)
