7d3ffdbf0dae871869016cebf6f39db8ed6a2eeb
pawel <pawelb.lnx@gmail.com>
2017-12-09 19:32:01 -0500

    fix typo

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -2,7 +2,7 @@
 (require-package 'js2-mode)
 
 (add-hook 'coffee-mode-hook 'subword-mode)
-(add-hook 'js-2-mode-hook 'subword-mode)
+(add-hook 'js2-mode-hook 'subword-mode)
 
 (setq js-indent-level 2)
 (setq js2-basic-offset 2)
