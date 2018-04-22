3f85461fd7e6cea8225024e10f84b6f1b0517ce5
pawel <pawelb.lnx@gmail.com>
2018-04-21 20:14:03 -0400

    make js2 mode default and add prettier hook

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,8 +1,11 @@
 (require-package 'coffee-mode)
 (require-package 'js2-mode)
 
+(add-auto-mode 'js2-mode "\\.js\\'")
+
 (add-hook 'coffee-mode-hook 'subword-mode)
 (add-hook 'js2-mode-hook 'subword-mode)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
 
 (setq js-indent-level 2)
 (setq js2-basic-offset 2)
