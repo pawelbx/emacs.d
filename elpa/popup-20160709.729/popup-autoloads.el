391db952fe2c2f29ed63931b2cd01ce0bf8e853a
pawel <pawelb.lnx@gmail.com>
2018-10-21 21:13:52 -0400

    delete old packages

--- elpa/popup-20160709.729/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("popup.el") (22639 3267 181450 687000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
