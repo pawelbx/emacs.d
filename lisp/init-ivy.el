3709bc1b3f2e9a3c32b1257a7e1eb039092cdcbd
Pawel Bokota <pawelb.lnx@gmail.com>
2019-01-23 13:42:32 -0500

    use regex plus for ag

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -13,6 +13,7 @@
 
      (setq ivy-re-builders-alist
            '((swiper . ivy--regex-plus)
+             (counsel-ag . ivy--regex-plus)
              (t . ivy--regex-fuzzy)))
 
      (global-set-key (kbd "C-s") 'swiper)
