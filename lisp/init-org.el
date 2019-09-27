4f7851afd278d7015f317cf7e6d33010f792c993
Pawel Bokota <pawelb.lnx@gmail.com>
2019-09-27 17:04:17 -0400

    use dropbox directories

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,13 +12,13 @@
 (setq org-capture-templates
       '(("m" "entry for meditation journal" entry (file "~/text/personal/meditation/journal.org")
          "\n* %u\n :PROPERTIES:\n :TIME: %? minutes\n :END:\n** Notes\n - ")
-        ("n" "daily work log" entry (file+datetree "~/text/work/daily-log.org")
+        ("n" "daily work log" entry (file+datetree "~/Dropbox/work/daily-log.org")
          "* %U - %?")
-        ("o" "daily life log" entry (file+datetree "~/text/personal/daily-life.org.gpg")
+        ("o" "daily life log" entry (file+datetree "~/Dropbox/life/daily-life.org.gpg")
          "* %U - %?")
-        ("l" "entry for life todo list" entry (file+headline "~/text/personal/life.org" "Inbox")
+        ("l" "entry for life todo list" entry (file+headline "~/Dropbox/life/life.org" "Inbox")
          "\n* TODO %?")
-        ("w" "entry for work todo list" entry (file+headline "~/text/work/work.org" "Inbox")
+        ("w" "entry for work todo list" entry (file+headline "~/Dropbox/work/work.org" "Inbox")
          "** TODO %?")
         ))
 
