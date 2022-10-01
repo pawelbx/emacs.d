3f8263cd13454bc265728f3b6b7d3cd68971602d
Pawel Bokota <pawel@home.local>
2022-09-30 20:44:26 -0400

    disable for now

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,10 +1,10 @@
 (require-package 'haskell-mode)
-(require-package 'intero)
+;(require-package 'intero)
 
 (require 'haskell-mode)
-(require 'intero)
+;(require 'intero)
 
-(after-load "haskell-mode"
-  (add-hook 'haskell-mode-hook 'intero-mode))
+;(after-load "haskell-mode"
+;  (add-hook 'haskell-mode-hook 'intero-mode))
 
 (provide 'init-haskell)
